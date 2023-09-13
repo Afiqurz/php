b7fef0c3e8be7247d79b8ea3b9997500248be15f
Afiqurz <121597410+Afiqurz@users.noreply.github.com>
2023-09-13 14:05:37 +0700

    Update and rename data siswa to config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$koneksi = mysqli_connect('localhost','root','','db_simagang');
+// pengecekan koneksi
+// if (!$koneksi){
+//     die ("Koneksi Gagal : ".mysqli_connect_error());
+
+// }
+// else 
+//     echo "Koneksi berhasil";
